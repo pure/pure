49be975452c868b18f37362817ecae56806071aa
Mic <pure@beebole.com>
2011-03-04 09:42:52 +0100

    update basic example for node.js

--- node/test.js
+++ node/test.js
@@ -13,4 +13,6 @@ http.createServer(function (req, res) {
 
 
 }).listen(1234, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1234/');
+console.log('Server running at http://127.0.0.1:1234/'); 
+
+
